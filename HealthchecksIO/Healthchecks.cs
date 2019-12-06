1e004bfa5815a6b73538e6cd09a777f81f232d98
Irek Romaniuk <iromaniuk@advisor360.com>
2019-12-06 10:58:16 -0500

    Add project files.

---
+++ HealthchecksIO/Healthchecks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthchecksIO
+{
+    public partial class Healthchecks : ServiceBase
+    {
+        public Healthchecks()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("HealthchecksIOSource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "HealthchecksIOSource", "HealthchecksIOLog");
+            }
+            eventLog1.Source = "HealthchecksIOSource";
+            eventLog1.Log = "HealthchecksIOLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("HealthchecksIO started");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("HealthchecksIO stopped");
+        }
+    }
+}
