e642cc025c725e3fa3a4742b16c7e2a4f1f96827
Irek Romaniuk <irek.romaniuk@gmail.com>
2019-12-07 10:00:09 -0500

    Inno script added

--- HealthchecksIO/Healthchecks.cs
+++ HealthchecksIO/Healthchecks.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using log4net;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -6,12 +7,16 @@ using System.Diagnostics;
 using System.Linq;
 using System.ServiceProcess;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace HealthchecksIO
 {
     public partial class Healthchecks : ServiceBase
     {
+        ILog logger;
+        Timer healthcheck;
+
         public Healthchecks()
         {
             InitializeComponent();
@@ -28,11 +33,42 @@ namespace HealthchecksIO
         protected override void OnStart(string[] args)
         {
             eventLog1.WriteEntry("HealthchecksIO started");
+            ConfigureLog4Net();
+            int timer = Properties.Settings.Default.Timer;
+            logger.Debug(string.Format("Timer is: {0}", timer));
+            healthcheck = new Timer(HealthcheckCallback, healthcheck, timer, timer);            
         }
 
         protected override void OnStop()
         {
             eventLog1.WriteEntry("HealthchecksIO stopped");
+        }
+        private void ConfigureLog4Net()
+        {
+            try
+            {
+                log4net.Config.XmlConfigurator.Configure();
+                logger = LogManager.GetLogger("servicelog");
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(ex.Message, EventLogEntryType.Error);
+            }
+        }
+
+        public void HealthcheckCallback(object objParam)
+        {
+            //System.Diagnostics.Debug.WriteLine(stri
+            
+            
+            
+            ng.Format());
+            logger.Debug(string.Format("Calling {0}", "https://hc-ping.com/" + Properties.Settings.Default.API));
+            using (var client = new System.Net.WebClient())
+            {
+                client.DownloadString("https://hc-ping.com/"+ Properties.Settings.Default.API);                
+            }
+
         }
     }
 }
